2c07beaa84a48d9251245475edfa304c3431e97c
synergy1990 <117905818+synergy1990@users.noreply.github.com>
2024-10-06 15:50:40 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,7 @@ umask ${UMASK}
 echo "---Taking ownership of data...---"
 chown -R root:${GID} /opt/scripts
 chmod -R 750 /opt/scripts
-chown -R ${UID}:${GID} ${DATA_DIR}
+chown -R ${UID}:${GID} /opt/acserver
 
 echo "---Starting...---"
 term_handler() {
