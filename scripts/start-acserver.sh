f95b6201af896391184e29e166fdca24207cfe05
synergy1990 <117905818+synergy1990@users.noreply.github.com>
2024-10-06 18:05:40 +0200

    Update start-acserver.sh

--- scripts/start-acserver.sh
+++ scripts/start-acserver.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 if [ ! -f /opt/acserver/server.sh ]; then
-    echo "AssaultCube server files not found! Downloading!"
-    wget -q -O /opt/acserver/acserver_linux.tar.bz2 https://github.com/assaultcube/AC/releases/download/v1.3.0.2/AssaultCube_v1.3.0.2_LockdownEdition_RC1.tar.bz2
+    echo "AssaultCube server files not found!"
+    if [ ! -f /opt/acserver/acserver_linux.tar.bz2 ]; then
+        echo "Downloading..."
+        wget -q -O /opt/acserver/acserver_linux.tar.bz2 https://github.com/assaultcube/AC/releases/download/v1.3.0.2/AssaultCube_v1.3.0.2_LockdownEdition_RC1.tar.bz2
+    fi
     cd /opt/acserver
-    tar --directory /opt/acserver -xvjf /opt/acserver/acserver_linux.tar.bz2
+    tar --directory /opt/acserver -xvjf  /opt/acserver/acserver_linux.tar.bz2
     rm /opt/acserver/acserver_linux.tar.bz2
 fi
 echo "AssaultCube server files should now be in /opt/acserver"
 
+GAME_PARAMS="-c${PLAYERCOUNT} -n${SERVER_DESCRIPTION} -o${SERVER_MODT} -x${MASTER_PASSWORD}"
+
 echo "---Starting Server---"
 cd /opt/acserver
 chmod +x /opt/acserver/server.sh
-/opt/acserver/server.sh ${GAME_PARAMS}
+echo "Launch parameters are: ${GAME_PARAMS}"
+/opt/acserver/server.sh $GAME_PARAMS
